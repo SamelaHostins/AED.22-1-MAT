e6314558f6d0ff97d1a22b88551a632e5846f595
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-04-05 09:37:40 -0300

    FilaVetor

---
+++ lineares/PilhaDinamica.java
@@ -0,0 +1,52 @@
+package lineares;
+
+public class PilhaDinamica<T> implements Pilha<T> {
+	private NoLista<T> topo;
+
+	@Override
+	public void push(T v) {
+		NoLista<T> novo = new NoLista<>();
+		novo.setInfo(v);
+		novo.setProximo(this.topo);
+		this.topo = novo;
+	}
+
+	@Override
+	public T pop() {
+		T elemento = this.peek();
+		this.topo = this.topo.getProximo();
+		return elemento;
+	}
+
+	@Override
+	public T peek() {
+		if (estaVazia()) {
+			throw new RuntimeException("Pilha vazia.");
+		}
+		return this.topo.getInfo();
+	}
+
+	@Override
+	public boolean estaVazia() {
+		return (this.topo == null);
+	}
+
+	@Override
+	public void liberar() {
+		this.topo = null;
+	}
+
+	@Override
+	public String toString() {
+		String elementos = "";
+
+		NoLista<T> t = this.topo;
+		while (t != null) {
+			elementos += t.getInfo()+", ";
+		}
+		elementos = elementos.substring(0, Math.max(0,elementos.length()-2));
+		return "topo[" + elementos + "]base";
+	}
+
+	
+}
--- lineares/TestePilhas.java
+++ lineares/TestePilhas.java
@@ -3,37 +3,72 @@ package lineares;
 public class TestePilhas {
 
 	public static void main(String[] args) {
-		PilhaVetor<Integer> pilhaVetor = new PilhaVetor<Integer>(3);
-		pilhaVetor.push(2);
-		pilhaVetor.push(3);
-		pilhaVetor.push(1);
+		System.out.println("Pilha Estática ++++++");
+		Pilha<Integer> pilha = new PilhaVetor<Integer>(3);
+		pilha.push(2);
+		pilha.push(3);
+		pilha.push(1);
 		try {
-			pilhaVetor.push(1);
+			pilha.push(1);
 		} catch (Exception e) {
 			System.out.println("Deu certo!!!!");
 		}
-		System.out.println(pilhaVetor.toString());
+		System.out.println(pilha.toString());
 
-		System.out.println(pilhaVetor.pop());
-		System.out.println(pilhaVetor.peek());
-		System.out.println(pilhaVetor.pop());
-		System.out.println(pilhaVetor.peek());
-		System.out.println(pilhaVetor.pop());
+		System.out.println(pilha.pop());
+		System.out.println(pilha.peek());
+		System.out.println(pilha.pop());
+		System.out.println(pilha.peek());
+		System.out.println(pilha.pop());
 		try {
-			System.out.println(pilhaVetor.peek());
+			System.out.println(pilha.peek());
 		} catch (Exception e) {
 			System.out.println("Deu certo!!!!");
 		}
 		try {
-			System.out.println(pilhaVetor.pop());
+			System.out.println(pilha.pop());
 		} catch (Exception e) {
 			System.out.println("Deu certo!!!!");
 		}
-		pilhaVetor.push(20);
-		pilhaVetor.push(30);
-		pilhaVetor.push(10);
-		System.out.println(pilhaVetor.toString());
-		pilhaVetor.liberar();
-		System.out.println(pilhaVetor.toString());
+		pilha.push(20);
+		pilha.push(30);
+		pilha.push(10);
+		System.out.println(pilha.toString());
+		pilha.liberar();
+		System.out.println(pilha.toString());
+
+		System.out.println("\n --- Agora com Pilha Dinâmica ----");
+		pilha = new PilhaVetor<Integer>(3);
+		pilha.push(2);
+		pilha.push(3);
+		pilha.push(1);
+		try {
+			pilha.push(1);
+		} catch (Exception e) {
+			System.out.println("Deu certo!!!!");
+		}
+		System.out.println(pilha.toString());
+
+		System.out.println(pilha.pop());
+		System.out.println(pilha.peek());
+		System.out.println(pilha.pop());
+		System.out.println(pilha.peek());
+		System.out.println(pilha.pop());
+		try {
+			System.out.println(pilha.peek());
+		} catch (Exception e) {
+			System.out.println("Deu certo!!!!");
+		}
+		try {
+			System.out.println(pilha.pop());
+		} catch (Exception e) {
+			System.out.println("Deu certo!!!!");
+		}
+		pilha.push(20);
+		pilha.push(30);
+		pilha.push(10);
+		System.out.println(pilha.toString());
+		pilha.liberar();
+		System.out.println(pilha.toString());
 	}
 }
