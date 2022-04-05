472ab34b1e2f3465f8813cbcb0b6311023be903c
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-04-05 10:42:01 -0300

    FilaLista

--- lineares/TesteFilas.java
+++ lineares/TesteFilas.java
@@ -3,31 +3,31 @@ package lineares;
 public class TesteFilas {
 	public static void main(String[] args) {
 
-		FilaVetor<Integer> filaVetor1 = new FilaVetor<Integer>(5);
+		Fila<Integer> fila1 = new FilaVetor<Integer>(5);
 
-		filaVetor1.inserir(1);
-		System.out.println(filaVetor1.toString());
-		filaVetor1.inserir(2);
-		System.out.println(filaVetor1.toString());
-		filaVetor1.inserir(3);
-		System.out.println(filaVetor1.toString());
-		filaVetor1.inserir(4);
-		System.out.println(filaVetor1.toString());
-		filaVetor1.inserir(5);
-		System.out.println(filaVetor1.toString());
-		filaVetor1.retirar();
-		System.out.println(filaVetor1.toString());
-		filaVetor1.retirar();
-		System.out.println(filaVetor1.toString());
-		filaVetor1.retirar();
-		System.out.println(filaVetor1.toString());
+		fila1.inserir(1);
+		System.out.println(fila1.toString());
+		fila1.inserir(2);
+		System.out.println(fila1.toString());
+		fila1.inserir(3);
+		System.out.println(fila1.toString());
+		fila1.inserir(4);
+		System.out.println(fila1.toString());
+		fila1.inserir(5);
+		System.out.println(fila1.toString());
+		fila1.retirar();
+		System.out.println(fila1.toString());
+		fila1.retirar();
+		System.out.println(fila1.toString());
+		fila1.retirar();
+		System.out.println(fila1.toString());
 
 		FilaVetor<Integer> filaVetor2 = new FilaVetor<Integer>(5);
 		filaVetor2.inserir(9);
 		filaVetor2.inserir(7);
 		filaVetor2.inserir(8);
 
-		FilaVetor<Integer> filaVetor3 = filaVetor1.concatenar(filaVetor2);
+		FilaVetor<Integer> filaVetor3 = ((FilaVetor)fila1).concatenar(filaVetor2);
 		System.out.println(filaVetor3.toString());
 		filaVetor3.retirar();
 		filaVetor3.retirar();
@@ -39,14 +39,40 @@ public class TesteFilas {
 		filaVetor3.inserir(-3);
 		System.out.println(filaVetor3.toString());
 
-		filaVetor1.inserir(4);
-		System.out.println(filaVetor1.toString());
-		filaVetor1.inserir(5);
-		System.out.println(filaVetor1.toString());
+		fila1.inserir(4);
+		System.out.println(fila1.toString());
+		fila1.inserir(5);
+		System.out.println(fila1.toString());
 
-		filaVetor1.liberar();
-		System.out.println(filaVetor1.toString());
+		fila1.liberar();
+		System.out.println(fila1.toString());
 		System.out.println(filaVetor2.toString());
 		System.out.println(filaVetor3.toString());
+		
+		
+		
+		System.out.println("\n========Fila Dinâmica=====");
+		fila1 = new FilaLista<Integer>();
+
+		fila1.inserir(1);
+		System.out.println(fila1.toString());
+		fila1.inserir(2);
+		System.out.println(fila1.toString());
+		fila1.inserir(3);
+		System.out.println(fila1.toString());
+		fila1.inserir(4);
+		System.out.println(fila1.toString());
+		fila1.inserir(5);
+		System.out.println(fila1.toString());
+		fila1.retirar();
+		System.out.println(fila1.toString());
+		fila1.retirar();
+		System.out.println(fila1.toString());
+		fila1.retirar();
+		System.out.println(fila1.toString());
+		fila1.inserir(4);
+		System.out.println(fila1.toString());
+		fila1.inserir(5);
+		System.out.println(fila1.toString());
 	}
 }
